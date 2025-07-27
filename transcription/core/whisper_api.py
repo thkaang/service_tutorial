bd27e5d9db1bc55b62d0947be4ab829e9447d9b3
thkaang <kas2426@naver.com>
2025-07-27 22:32:10 +0900

    adding whisper api

--- transcription/core/whisper_api.py
+++ transcription/core/whisper_api.py
@@ -5,7 +5,16 @@ class Whisper:
     def __init__(self, type="turbo"):
         print("whisper model loading...")
         self.model = whisper.load_model(type)
+        self.options = whisper.DecodingOptions()
         print("whisper model is loaded.")
+
+    def load_audio(self, path):
+        return whisper.load_audio(path)
+
+    def decode(self, audio_segment):
+        audio_segment = whisper.pad_or_trim(audio_segment)
+        mel = whisper.log_mel_spectrogram(audio_segment, n_mels=self.model.dims.n_mels).to(self.model.device)
+        return whisper.decode(self.model, mel, self.options).text
 
     def transcribe(self, path, language=None):
         result = self.model.transcribe(path, language=language)
